65cc9436a4e74a4ea0389a504730fbe6bf0e4421
Muna Chimoriya <muna.chimoriya.2022@student.lhschools.org>
2020-02-28 12:07:25 -0500

    intellij Commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,12 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("My name is Muna. ");
+        System.out.println("This summer I moved from PA to Ohio, ");
+        System.out.println("I got a job, ");
+        System.out.println("and I went swimming with my friends");
+    }
+}
